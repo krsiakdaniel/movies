2c359ee69a31322db41afc78de029dff9d53fc39
Krsiak Daniel <krsiak.daniel@gmail.com>
2021-04-29 22:44:07 +0200

    Create getMovies function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import SearchMovies from './SearchMovies';
+import { SearchMovies } from './SearchMovies';
 
 class App extends React.Component {
     render() {
         return (
             <div className="container flex">
-                <h1 className="title">React Movie Search</h1>
+                <h1 className="title">Movies Search</h1>
                 <SearchMovies />
             </div>
         );
--- src/SearchMovies.tsx
+++ src/SearchMovies.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 
-export default function SearchMovies() {
+export const SearchMovies = () => {
+
+    const getMovies = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault()
+
+        const BASE_URL = 'https://api.themoviedb.org/3/search/movie'
+        const API_KEY = '295bad0176d1e0739cd94f5f2594968b'
+
+        const lang = 'en-US'
+
+        let searchQuery = 'Jurassic Park'
+        let numberOfResultPages = 1
+        const searchUrl =`${BASE_URL}?api_key=${API_KEY}&language=${lang}&query=${searchQuery}&page=${numberOfResultPages}`
+
+        try {
+            const response = await fetch(searchUrl)
+            const data = await response.json()
+            console.log(data)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={getMovies}>
             <input className="input" type="text" name="query" placeholder="Movie Name"/>
             <button className="button" type="submit">Search</button>
         </form>
