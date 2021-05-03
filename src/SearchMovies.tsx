d0c2d8e989b5344fded58c01ffafbe47de56ae54
Krsiak Daniel <krsiak.daniel@gmail.com>
2021-05-03 10:45:17 +0200

    Fixed naming

--- src/SearchMovies.tsx
+++ src/SearchMovies.tsx
@@ -2,29 +2,28 @@ import React from 'react';
 
 export const SearchMovies = () => {
 
-    const getMovies = async (e: { preventDefault: () => void; }) => {
+    const getMoviesData = async (e: { preventDefault: () => void; }) => {
         e.preventDefault()
 
         const BASE_URL = 'https://api.themoviedb.org/3/search/movie'
         const API_KEY = '295bad0176d1e0739cd94f5f2594968b'
-
         const lang = 'en-US'
-
         let searchQuery = 'Jurassic Park'
         let numberOfResultPages = 1
-        const searchUrl =`${BASE_URL}?api_key=${API_KEY}&language=${lang}&query=${searchQuery}&page=${numberOfResultPages}`
+
+        const searchUrl = `${BASE_URL}?api_key=${API_KEY}&language=${lang}&query=${searchQuery}&page=${numberOfResultPages}`
 
         try {
             const response = await fetch(searchUrl)
             const data = await response.json()
             console.log(data)
-        } catch (err) {
-            console.error(err)
+        } catch (error) {
+            console.error(error)
         }
     }
 
     return (
-        <form className="form" onSubmit={getMovies}>
+        <form className="form" onSubmit={getMoviesData}>
             <input className="input" type="text" name="query" placeholder="Movie Name"/>
             <button className="button" type="submit">Search</button>
         </form>
